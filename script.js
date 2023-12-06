03ddb3bd30414ec1426bed74f808ae08dc3d418c
Mubashir Salim <mubashir.salim@msds.christuniversity.in>
2023-12-06 15:15:21 +0530

    upload necesory files

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to smoothly scroll to a section
+    function scrollToSection(id) {
+      const section = document.getElementById(id);
+      if (section) {
+        window.scrollTo({
+          behavior: "smooth",
+          top: section.offsetTop,
+        });
+      }
+    }
+
+    // Add click event listeners to the navigation links
+    document.querySelectorAll("nav a").forEach(function (link) {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        scrollToSection(targetId);
+      });
+    });
+  });
